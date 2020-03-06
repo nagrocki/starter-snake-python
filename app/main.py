6f87874883382a4ef32f2ee73ae62120cce0e136
Cob <jacobnagrocki@gmail.com>
2020-03-06 15:54:10 -0800

    implemented bfs

--- app/main.py
+++ app/main.py
@@ -13,21 +13,21 @@ from api import ping_response, start_response, move_response, end_response
         -alter behaviour based on hunger
         
 """
-
-def length_n_paths(square, length, dangerSquares, height, width):
-   '''
+"""
    returns all of the length n paths from square
    any given path contains no cycles, but two paths can share squares
-   '''
+"""
+    
+def length_n_paths(square, length, dangerSquares, height, width):
     directions = ["up", "down", "left", "right"]
     
     safeMoves = safe_moves(square, dangerSquares, height, width)
     paths = []
     for move in safeMoves:
-        startPath = [one_move(square, safeMoves)]
+        startPath = [one_move(square, move)]
         paths.append(startPath)
         
-    for i in range(length):
+    for i in range(length-1):
         longerPaths = []
         for path in paths:
             frontier = path[-1]
@@ -42,26 +42,73 @@ def length_n_paths(square, length, dangerSquares, height, width):
         
     return paths
 
+def BFS_dist(source, sink, maxLen, dangerSquares, height, width):
+    """
+       returns bfs search distance, no more than maxLen squares
+
+
+       TODO: return error instead of infinity?
+    """
+    directions = ["up", "down", "left", "right"]
+    
+    safeMoves = safe_moves(source, dangerSquares, height, width)
+    paths = []
+    discoveredSquares = []
+    for move in directions:
+        firstStep = one_move(source, move)
+        if firstStep == sink:
+            return 1    # returns 1 if sink is adjacent to source
+    
+    for move in safeMoves:
+        firstStep = one_move(source, move)
+        discoveredSquares.append(firstStep)
+        startPath = [firstStep]
+        paths.append(startPath)
+        
+    ## at this stage paths contains lists, each with a 'safe' adjacent square to the source    
+    for i in range(2, maxLen + 1):
+        longerPaths = []
+        for path in paths:
+            frontier = path[-1]    ## for each path, the frontier is the last visited square in the path
+            
+            for move in directions:
+                nextSquare = one_move(frontier, move)
+                if nextSquare is sink:
+                    return i
+                
+            safeMoves = safe_moves(frontier, dangerSquares, height, width)
+            for move in safeMoves:
+                nextSquare = one_move(frontier, move)
+                if nextSquare not in discoveredSquares:
+                    newPath = list(path)
+                    newPath.append(nextSquare)
+                    discoveredSquares.append(nextSquare)
+                    longerPaths.append(newPath)
+        paths = longerPaths               ## next time through the loop, we will build off new longer paths
+        if len(paths) == 0:
+            return float("inf")    ##if no paths from source to sink, distance = infinity
+    return i    ## returns maxLen if no path found before maxLen
+
 def score_path(path, scarySneks, yummySneks, foods):
     score = 0
     for square in path:
-        score = score + square_score(square, scarySneks, yummySneks, foods):
+        score = score + square_score(square, scarySneks, yummySneks, foods)
             
             ##TODO update square score to measure distance wth distace matric built by BFS search
     
 
 def snek_dist(sq1,sq2):
-    '''
-    takes in two x,y tuples and returns taxicab distance
-    '''
+    """
+    takes in two x,y tuples and returns taxicab distance (totally ignores snakes)
+    """
     dx = abs(sq1["x"]-sq2["x"])
     dy = abs(sq1["y"]-sq2["y"])
     return dx + dy
 
 def one_move(square, direction):
-    '''
+    """
     takes in a square and a direction and returns the square one step in that direction
-    '''
+    """
     newSquare = {"x": 0, "y":0}
     if direction == "up":
         newSquare["x"] = square["x"]
@@ -78,13 +125,13 @@ def one_move(square, direction):
     return newSquare
 
 def square_is_safe(square, dangerSquares, height, width):
-    '''
+    """
     takes in a square and the danger squares and the height and width of the 
     data["height"]xdata["width"] grid,
     returns True if square is potentially a safe move for the next turn.
     
     *This function does not check for squares adjacent to enemy snake heads*
-    '''
+    """
     safe = True
     for dSquare in dangerSquares:
         if dSquare["x"] == square["x"] and dSquare["y"] == square["y"]:
@@ -93,7 +140,7 @@ def square_is_safe(square, dangerSquares, height, width):
         safe = False
     return safe
 
-def save_moves(currentSquare, dangerSquares, height, width)
+def save_moves(currentSquare, dangerSquares, height, width):
     safeMoves = []
     directions = ['up', 'down', 'left', 'right']
     for move in directions:
@@ -102,13 +149,45 @@ def save_moves(currentSquare, dangerSquares, height, width)
             safeMoves.append(move)
     
     return safeMoves
-               
-               
-def square_score(square, scarySneks, yummySneks, foods):
-    '''
+
+
+def bfs_square_score(square, scarySneks, yummySneks, foods, dangerSquares, height, width):
+    """
     This functon scores a square based on how close it is to food, bigger snakes,
     and smaller snakes. A higher score should correspond to a better move.
-    '''
+    """
+    
+    score = 0
+    for snek in scarySneks:
+        if BFS_dist(square, snek[0], 10, dangerSquares, height, width) == 1:
+            score = score - 4
+        else:
+            score = score - 4/(BFS_dist(square, snek[0], 10, dangerSquares, height, width))**2 #run from head
+            
+        if BFS_dist(square, snek[0], 10, dangerSquares, height, width) == 0:                  #run to tail
+            score = score + 3
+        elif BFS_dist(square, snek[0], 10, dangerSquares, height, width) == 1:
+            score = score + 1   
+            
+            
+    for snek in yummySneks:
+        if BFS_dist(square, snek[0], 10, dangerSquares, height, width) == 1:
+            score = score + 4
+        else:
+            score = score + 4/(BFS_dist(square, snek[0], 10, dangerSquares, height, width))**2
+
+    for food in foods:
+        if BFS_dist(square, food, 10, dangerSquares, height, width) == 0:
+            score = score + 5
+        else:
+            score = score + 4/BFS_dist(square, food, 10, dangerSquares, height, width)
+    return score
+               
+def square_score(square, scarySneks, yummySneks, foods):
+    """
+    This functon scores a square based on how close it is to food, bigger snakes,
+    and smaller snakes. A higher score should correspond to a better move.
+    """
     
     score = 0
     for snek in scarySneks:
